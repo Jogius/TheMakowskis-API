0639d52709bc0212e277739ef30cc424237de0ba
Julius Makowski <jogius.julius.makowski@gmail.com>
2023-01-01 12:51:07 +0100

    Added 'wallbox' endpoint

---
+++ src/wallbox/add.php
@@ -0,0 +1,62 @@
+<?php
+// Load composer libraries
+require "../vendor/autoload.php";
+// Load other dependencies
+require "../utils/DatabaseConnector.php";
+
+// Set response headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+// Initialize values from `.env`
+(Dotenv\Dotenv::createImmutable(__DIR__))->load();
+
+// Initialize database connection
+$initStatement = "
+CREATE TABLE IF NOT EXISTS data (
+  id INT NOT NULL AUTO_INCREMENT,
+  start TIMESTAMP NOT NULL,
+  end TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  energy INT NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=INNODB;
+";
+$dbConnection = (new DatabaseConnector($initStatement))->getConnection();
+
+$data = json_decode(file_get_contents("php://input"));
+
+if (
+  !isset($data->token) ||
+  strcmp($data->token, $_ENV["TOKEN"]) != 0
+) {
+  http_response_code(401);
+  echo json_encode(array("message" => "Invalid Token."));
+  return;
+}
+
+// Set data to null if unset
+if (!isset($data->start)) $data->start = null;
+if (!isset($data->end)) $data->end = null;
+if (!isset($data->energy)) $data->energy = null;
+
+try {
+  $query = "INSERT INTO data(start, end, energy) VALUES(:start, :end, :energy);";
+
+  $statement = $dbConnection->prepare($query);
+  $statement->bindParam(":start", $data->start);
+  $statement->bindParam(":end", $data->end);
+  $statement->bindParam(":energy", $data->energy);
+
+  $success = $statement->execute();
+
+  if ($success) {
+    http_response_code(201);
+    echo json_encode(array("message" => "Success."));
+  } else {
+    http_response_code(403);
+    echo json_encode(array("message" => "Internal error."));
+  }
+} catch (PDOException $e) {
+  http_response_code(400);
+  echo json_encode(array("message" => $e->getmessage()));
+}
---
+++ src/wallbox/find.php
@@ -0,0 +1,67 @@
+<?php
+// Load composer libraries
+require "../vendor/autoload.php";
+// Load other dependencies
+require "../utils/DatabaseConnector.php";
+
+// Set response headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+// Initialize values from `.env`
+(Dotenv\Dotenv::createImmutable(__DIR__))->load();
+
+// Initialize database connection
+$dbConnection = (new DatabaseConnector())->getConnection();
+
+// Check for `from` and `to` query parameters
+$params = array();
+parse_str($_SERVER["QUERY_STRING"], $params);
+if (
+  isset($params["from"]) &&
+  isset($params["to"])
+)
+{
+  $range = true;
+  $from = $params["from"];
+  $to = $params["to"];
+}
+else
+{
+  $range = false;
+}
+
+try
+{
+  if ($range)
+	{
+    $query = "SELECT * FROM data WHERE start BETWEEN FROM_UNIXTIME(:from) AND FROM_UNIXTIME(:to) ORDER BY start ASC;";
+
+    $statement = $dbConnection->prepare($query);
+    $statement->bindParam(":from", $from);
+    $statement->bindParam(":to", $to);
+  }
+	else
+	{
+    $query = "SELECT * FROM data ORDER BY start ASC;";
+
+    $statement = $dbConnection->query($query);
+  }
+
+  $success = $statement->execute();
+
+	if ($success)
+	{
+    $data = $statement->fetchAll();
+    echo json_encode($data);
+  }
+	else
+	{
+    http_response_code(403);
+    echo json_encode(array("message" => "Internal error."));
+  }
+}
+catch (PDOException $e) {
+  http_response_code(400);
+  echo json_encode(array("message" => $e->getMessage()));
+}
